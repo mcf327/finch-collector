8fb3347c16606e72ec6ebbe503003b3678ece25d
mcf327 <mcf327@gmail.com>
2023-08-07 07:06:50 -0400

    first commit

---
+++ main_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+finches = [
+  {'name': 'Lolo', 'breed': 'Eurasian bullfinch', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'American goldfinch', 'description': 'gentle and loving', 'age': 2},
+]
+# Create your views here.
